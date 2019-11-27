cc0db60d95b1addf32cc31abfef8b2510c5f4f9c
roaslin <roaslin@gmail.com>
2019-11-27 14:44:22 +0100

    is divisible by 400

--- test/LeapYear.test.js
+++ test/LeapYear.test.js
@@ -1,5 +1,8 @@
 import LeapYear from "../src/LeapYear"
 
 describe('LeapYear Should', () => {
-
+  it('return true when year is divisible by 400', () => {
+    const leapYear = new LeapYear()
+    expect(leapYear.check(2400)).toBeTruthy()
+  })
 })
