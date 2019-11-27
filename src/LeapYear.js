04b7957c519d77e302fffcd6305220217ab5ebb1
roaslin <roaslin@gmail.com>
2019-11-27 15:04:34 +0100

    refactor

--- src/LeapYear.js
+++ src/LeapYear.js
@@ -1,10 +1,18 @@
 export default class LeapYear {
     check(year) {
-        if (year % 4 == 0 && year % 100 != 0) {
+        if (isDivisibleBy4(year) && isNotDivisibleBy100(year)) {
             return true
         }
         return isDivisibleBy400(year);
     }
+}
+
+function isNotDivisibleBy100(year) {
+    return year % 100 != 0;
+}
+
+function isDivisibleBy4(year) {
+    return year % 4 == 0;
 }
 
 function isDivisibleBy400(year) {
