2ec7722fd167321f994bdac168b50e30663f3e3c
roaslin <roaslin@gmail.com>
2019-11-27 14:39:54 +0100

    check not leap years

--- test/LeapYear.test.js
+++ test/LeapYear.test.js
@@ -5,6 +5,10 @@ describe('LeapYear Should', () => {
     const leapYear = new LeapYear()
     expect(leapYear.check(2400)).toBeTruthy()
     expect(leapYear.check(2000)).toBeTruthy()
-
+  })
+  it('return false when is not leapYear', () => {
+    const leapYear = new LeapYear()
+    expect(leapYear.check(2017)).toBeFalsy()
+    expect(leapYear.check(2018)).toBeFalsy()
   })
 })
