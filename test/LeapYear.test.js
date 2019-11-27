3ddc8c44382097b40065bc79577b69b1718c54f0
roaslin <roaslin@gmail.com>
2019-11-27 14:34:58 +0100

    refaactor

--- test/LeapYear.test.js
+++ test/LeapYear.test.js
@@ -4,10 +4,7 @@ describe('LeapYear Should', () => {
   it('return true when is leapYear', () => {
     const leapYear = new LeapYear()
     expect(leapYear.check(2400)).toBeTruthy()
-  })
-
-  it('return true when checking 2020', () => {
-    const leapYear = new LeapYear()
     expect(leapYear.check(2000)).toBeTruthy()
+
   })
 })
