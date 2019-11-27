7eec5f212a0702fc0b25fec5b61053add40a56d2
roaslin <roaslin@gmail.com>
2019-11-27 14:23:37 +0100

    first commit:

--- src/LeapYear.js
+++ src/LeapYear.js
@@ -1,3 +1,5 @@
 export default class LeapYear {
-
+    check(year){
+        return year == 2016;
+    }
 }
--- test/LeapYear.test.js
+++ test/LeapYear.test.js
@@ -1,5 +1,8 @@
 import LeapYear from "../src/LeapYear"
 
 describe('LeapYear Should', () => {
-
+  it('return true when check year 2016', () => {
+    const leapYear = new LeapYear()
+    expect(leapYear.check(2016)).toBeTruthy()
+  })
 })
